a98f68ab2e86b801597d1fe957c122666d50ebd6
Your Name <venkatkiran343@gmail.com>
2025-07-30 01:49:30 +0530

    added npm install

--- startup.sh
+++ startup.sh
@@ -3,6 +3,10 @@
 # Go to app directory (this is where Azure deploys your code)
 cd /home/site/wwwroot
 
+# Install dependencies
+echo "Installing Node.js dependencies..."
+npm install --omit=dev
+
 # Start the Node.js app
 echo "Starting Node.js app..."
 npm start
